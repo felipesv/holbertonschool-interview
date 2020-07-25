5ef1b9bc866ae1d83a7cef6a8b799f6352bf5c41
Felipe Satizabal <1165@holbertonschool.com>
2020-07-25 12:44:55 -0500

    [fix] validate negative numbers

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -21,8 +21,9 @@ listint_t *insert_node(listint_t **head, int number)
 	new_node->n = number;
 	new_node->next = NULL;
 
-	if (current == NULL)
+	if (current == NULL || number < current->n)
 	{
+		new_node->next = current;
 		*head = new_node;
 		return (new_node);
 	}

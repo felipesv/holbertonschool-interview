c63bf304a9280de5d50d0ee2a2e3b453c0d0bbfc
Felipe Satizabal <1165@holbertonschool.com>
2020-10-07 19:15:42 -0500

    [fix] betty

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -6,28 +6,31 @@
  *
  * Return: is a void
  */
-void menger(int level) {
+void menger(int level)
+{
 	int size = (int) pow(3, (double) level);
 	int cnt1 = 0, cnt2 = 0, _cnt1 = 0, _cnt2 = 0;
 	char character = '#';
 
-	if (level < 0) 
+	if (level < 0)
 		return;
 
-	for (cnt1 = 0; cnt1 < size; ++cnt1) {
-		for (cnt2 = 0; cnt2 < size; ++cnt2) {
+	for (cnt1 = 0; cnt1 < size; ++cnt1)
+	{
+		for (cnt2 = 0; cnt2 < size; ++cnt2)
+		{
 			_cnt1 = cnt1;
 			_cnt2 = cnt2;
+			character = '#';
+
 			while (_cnt1 && _cnt2)
 			{
 				if (_cnt1 % 3 == 1 && _cnt2 % 3 == 1)
 				{
 					character = ' ';
 					break;
-				} else {
-					character = '#';
 				}
-				
+
 				_cnt1 /= 3;
 				_cnt2 /= 3;
 			}

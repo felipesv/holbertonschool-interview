4f4a39539eb978122478ffca794bee14b8319d9a
Felipe Satizabal <1165@holbertonschool.com>
2020-12-09 11:36:53 -0500

    [fix]

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -9,16 +9,13 @@
  */
 void print_array(int *array, int from, int to)
 {
-	int cnt = 0;
-	char comma = '\0';
-
-	printf("Searching in array:");
-	for (cnt = from; cnt <= to; cnt++)
+	printf("Searching in array: ");
+	while (from < to)
 	{
-		printf("%c %i", comma, array[cnt]);
-		comma = ',';
+		printf("%i, ", array[from]);
+		from++;
 	}
-	printf("\n");
+	printf("%i\n", array[from]);
 }
 /**
  * recursion_binary - print the message
@@ -33,25 +30,17 @@ int recursion_binary(int *array, int from, int to, int value)
 {
 	int mid;
 
-	if (from > to)
-		return (-1);
-
 	print_array(array, from, to);
-
-	if (from == to && array[to] == value)
-		return (to);
-
-	if (from == to && array[to] != value)
+	mid = (to - from) / 2 + from;
+	if (from == to)
 		return (-1);
-
-	mid = from + (to - from) / 2;
-
-	if ((mid == from || value != array[mid - 1]) && array[mid] == value)
+	if (array[mid] == value && array[mid - 1] != value)
 		return (mid);
-
 	if (array[mid] >= value)
 		return (recursion_binary(array, from, mid, value));
-	return (recursion_binary(array, mid + 1, to, value));
+	if (array[mid] <= value)
+		return (recursion_binary(array, mid + 1, to, value));
+	return (-1);
 }
 /**
  * advanced_binary - print the message
@@ -64,7 +53,7 @@ int recursion_binary(int *array, int from, int to, int value)
 int advanced_binary(int *array, size_t size, int value)
 {
 
-	if (!array || size == 0)
+	if (!array)
 		return (-1);
 
 	return (recursion_binary(array, 0, size - 1, value));
